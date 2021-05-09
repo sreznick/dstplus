99c99e8f34cf1557dd3a294c1d2707ae4a0dff4c
Sergey Reznick <sergey.reznick@gmail.com>
2021-05-09 08:56:56 +0300

    added pip related stuff

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'dstplus',
+  packages = ['dstplus'],
+  version = '0.1',
+  license='MIT',
+  description = 'Data structures and algorithms going beyond standard library capabilities',
+  author = 'Sergey Reznick',
+  author_email = 'sergey.reznick@gmail.com',
+  url = 'https://github.com/sreznick/dstplus',
+  download_url = 'https://github.com/sreznick/dstplus/archive/refs/tags/v0.1-prealpha.tar.gz',
+  keywords = ['algorithms'],
+  install_requires=[
+  ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
+
