391a2fb251d92a3911d75eab0f04feee94ba52a6
Sergey Reznick <sergey.reznick@gmail.com>
2021-05-11 09:28:39 +0300

    Implemented BitSet.clear

--- dstplus/bitset.py
+++ dstplus/bitset.py
@@ -2,13 +2,16 @@
 class BitSet:
 
     def __init__(self):
-        self._flags = 0
+        self.clear()
 
     def add(self, value):
         self._flags |= (1 << value)
 
     def remove(self, value):
         self._flags &= ~(1 << value)
+
+    def clear(self):
+        self._flags = 0
 
     def __contains__(self, value):
         return (self._flags & (1 << value)) != 0
---
+++ test/test_bitset_clear.py
@@ -0,0 +1,42 @@
+from dstplus.bitset import BitSet
+
+
+def test_clear():
+    bset = BitSet()
+    bset.add(0)
+    bset.add(1)
+    bset.add(34)
+    assert 0 in bset
+    assert 1 in bset
+    assert 2 not in bset
+    assert 34 in bset
+
+    bset.clear()
+    assert 0 not in bset
+    assert 1 not in bset
+    assert 2 not in bset
+    assert 34 not in bset
+
+
+
+def test_clear_double():
+    bset = BitSet()
+    bset.add(0)
+    bset.add(1)
+    bset.add(34)
+
+    bset.clear()
+    bset.clear()
+    assert 0 not in bset
+    assert 1 not in bset
+    assert 2 not in bset
+    assert 34 not in bset
+
+
+def test_on_empty():
+    bset = BitSet()
+    bset.clear()
+    assert 0 not in bset
+    assert 1 not in bset
+    assert 2 not in bset
+    assert 34 not in bset
