f1c618cd92466fdf5b4f0146ffb1af0b9be34688
Sergey Reznick <sergey.reznick@gmail.com>
2021-05-11 09:43:26 +0300

    https://github.com/sreznick/dstplus/issues/9

--- dstplus/bitset.py
+++ dstplus/bitset.py
@@ -1,16 +1,43 @@
+"""
+This module contains impelemntation of set
+suitable for integer values.
+
+Right now it assumed that numbers are non-negative.
+
+It is also assumed that they are not too big. Othersize
+good performance is not guaranteed.
+"""
+
 
 class BitSet:
+    """
+    Implements set of non-negative integers as bitflags
+
+    It is assumed to be API-compatible with standard Python set
+    """
 
     def __init__(self):
+        """
+        Creates empty set
+        """
         self.clear()
 
     def add(self, value):
+        """
+        Adds element to set
+        """
         self._flags |= (1 << value)
 
     def remove(self, value):
+        """
+        Removes element from set
+        """
         self._flags &= ~(1 << value)
 
     def clear(self):
+        """
+        Removes all elements from set
+        """
         self._flags = 0
 
     def __contains__(self, value):
